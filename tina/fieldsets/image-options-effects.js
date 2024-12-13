32d5ae27e6433bfd56db552df200caad7b45a767
cod3blox <heinrich.kontny@gmail.com>
2024-12-13 16:04:36 +0100

    added more description

--- tina/fieldsets/image-options-effects.js
+++ tina/fieldsets/image-options-effects.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { wrapFieldsWithMeta } from 'tinacms'
 
-// This WILL NOT Build when shared between two diffent block tempaltes.
+// This WILL NOT Build when shared between two different block tempaltes.
 export const imageOptionsEffects = [
   {
     name: 'effects',
     type: 'object',
     label: 'Effects',
     fields: [
-      // At this point, we reach the max depth of nested object fields.
+      // At this point, we reach the max depth of nested object fields when shared.
       // The tina-lock.json generation begins to behave strange, leading to 'Checking local Tina schema matches server' errors!
+      // If you lookup the image-options-generative-ai.js you will see that it has the same object depth, but it builds fine because it is only used once.
       {
         name: 'optimizations',
         type: 'object',
