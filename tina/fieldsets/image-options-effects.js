1165b9ee5679ef8f283194c951c63be4731e21a7
cod3blox <heinrich.kontny@gmail.com>
2024-12-13 14:33:30 +0100

    verifying example

--- tina/fieldsets/image-options-effects.js
+++ tina/fieldsets/image-options-effects.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { wrapFieldsWithMeta } from 'tinacms'
 
-export const imageOptionsEffects = [
+// This WILL NOT Build 
+export const imageOptionsEffectsBreaking = [
   {
     name: 'effects',
     type: 'object',
     label: 'Effects',
     fields: [
+      // At this point, we reach the max depth of nested object fields when shared between two diffent block tempaltes. 
+      // The tina-lock.json generation gets crazy here resulting in "Checking local Tina Schema matches server." errors!!!
       {
         name: 'optimizations',
         type: 'object',
@@ -28,7 +31,6 @@ export const imageOptionsEffects = [
                       min='0'
                       max='100'
                       step='25'
-                      // This will pass along props.input.onChange to set our form values as this input changes.
                       {...input}
                     />
                     <br />
@@ -39,6 +41,42 @@ export const imageOptionsEffects = [
             },
           },
         ],
+      },
+    ],
+  },
+]
+
+// This one WILL Build.
+export const imageOptionsEffects = [
+  {
+    name: 'effects',
+    type: 'object',
+    label: 'Effects',
+    fields: [
+      {
+        name: 'improve',
+        type: 'number',
+        label: 'Auto Improve',
+        ui: {
+          parse: (val) => Number(val),
+          component: wrapFieldsWithMeta(({ input }) => {
+            return (
+              <div>
+                <input
+                  name='improve'
+                  id='improve'
+                  type='range'
+                  min='0'
+                  max='100'
+                  step='25'
+                  {...input}
+                />
+                <br />
+                Auto correction level: {input.value ? input.value : '0'}%
+              </div>
+            )
+          }),
+        },
       },
     ],
   },
--- tina/fieldsets/image-options-source.js
+++ tina/fieldsets/image-options-source.js
@@ -3,12 +3,10 @@ export const imageOptionsSource = [
     name: 'src',
     type: 'image',
     label: 'Image Source',
-    required: true,
   },
   {
     name: 'alt',
     type: 'string',
     label: 'Image Alt Text',
-    required: true,
   },
 ]
