d5e21e50f12badc61fec485ac0025a5a1d02f515
cod3blox <heinrich.kontny@gmail.com>
2024-12-13 13:48:36 +0100

    breaking the build by icluding shared schema

--- tina/blocks/image-block.js
+++ tina/blocks/image-block.js
@@ -4,17 +4,6 @@ export const imageBlockSchema = {
   name: 'imageBlock',
   label: 'Image',
   fields: [
-    {
-      name: 'src',
-      type: 'image',
-      label: 'Image Source',
-      required: true,
-    },
-    {
-      name: 'alt',
-      type: 'string',
-      label: 'Image Alt Text',
-      required: true,
-    },
+    ...imageFieldset
   ],
 }
